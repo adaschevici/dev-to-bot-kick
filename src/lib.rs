23ceb017c83c7155f6c9e8a5a108298f78fec119
Artur Das <a.daschevici@gmail.com>
2024-06-14 17:04:31 +0200

    working version of a Dioxus plugin that works
    - it uses dioxus v5.1 so it is most up to date

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,23 @@
 use dioxus::prelude::*;
+use std::time::Duration;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    dioxus_web::launch(app);
+    launch(app);
 }
 
-fn app(cx: Scope) -> Element {
-    let mut count = use_state(cx, || 0);
-
-    cx.render(rsx!(
+fn app() -> Element {
+    let mut count = use_signal_sync(|| 0);
+    std::thread::spawn(move || loop {
+        std::thread::sleep(Duration::from_millis(100));
+        println!("{count}");
+    });
+    rsx!(
         h1 { "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
-    ))
+    )
 }
